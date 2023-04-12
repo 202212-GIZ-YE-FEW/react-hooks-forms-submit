ecd2f5ee88b7da02e860823cb9a907fac919655c
Mohammed Al-jaberi <aljabri606@gmail.com>
2023-04-12 17:56:00 +0300

    Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,20 +3,54 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([]);
+  const [errors, setErrors] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
   }
+  function handleSubmit(event) {
+    event.preventDefault();
+    setErrors("")
+    if(firstName.length > 0){
+      const formData = {
+        firstName: firstName,
+        lastName: lastName,
+      };
+      setSubmittedData([...submittedData,formData]);
+      setFirstName("");
+      setLastName("");
+    }else{
+      setErrors(["First name is required!"])
+    }
+  }
+  console.log(submittedData);
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
 
   function handleLastNameChange(event) {
     setLastName(event.target.value);
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
+      {
+        errors.length > 0 ?
+          errors.map((error,index)=>{
+            return <p key={index}>{error}</p>
+          }):null
+      }
+      <h3>Submissions</h3>
+
+      {listOfSubmissions}
     </form>
   );
 }
